c8288439c507446037c84282aebf863f692081f1
Benjamin Peter <BenjaminPeter@arcor.de>
2008-10-03 17:04:20 +0200

    Closed the input -> process -> output chain.

---
+++ stdheaders.h
@@ -0,0 +1,20 @@
+#include <string>
+#include <iostream>
+#include <algorithm>
+
+#include <cctype>
+#include <cerrno>
+#include <cstring>
+
+// OS specific headers
+#include <unistd.h>
+#include <X11/Xlib.h>
+#include <X11/Xproto.h>
+#include <X11/X.h>
+#include <X11/extensions/Xevie.h>
+#include <X11/Xutil.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
+using namespace std;
