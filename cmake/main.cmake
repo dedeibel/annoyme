b8fab8b894f69626978378e0423b1644e6f29ff9
bpeter <bpeter@macbook-01.(none)>
2010-10-11 23:26:53 +0200

    Improved packaging. Started to add resource loader. Added eclipse files.

---
+++ cmake/main.cmake
@@ -0,0 +1,9 @@
+
+include("${CMAKE_FILES_DIR}/check_dependencies.cmake")
+include("${CMAKE_FILES_DIR}/select_audio.cmake")
+include("${CMAKE_FILES_DIR}/select_key_input.cmake")
+include("${CMAKE_FILES_DIR}/write_configuration.cmake")
+include("${CMAKE_FILES_DIR}/build_annoyme.cmake")
+include("${CMAKE_FILES_DIR}/install.cmake")
+include("${CMAKE_FILES_DIR}/package.cmake")
+
